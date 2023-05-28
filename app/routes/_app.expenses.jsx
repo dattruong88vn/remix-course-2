98b152b5456518b123f95201363141ee9362d841
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-05-28 17:59:05 +0700

    56. finish chapter 3

--- app/routes/_app.expenses.jsx
+++ app/routes/_app.expenses.jsx
@@ -1,4 +1,5 @@
-import { Outlet } from "@remix-run/react";
+import { Link, Links, Outlet } from "@remix-run/react";
+import { FaDownload, FaPlus } from "react-icons/fa";
 
 import ExpensesList from "~/components/expenses/ExpensesList";
 
@@ -22,6 +23,16 @@ function ExpensesLayout() {
     <>
       <main>
         <Outlet />
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add Expenses</span>
+          </Link>
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
         <ExpensesList expenses={DUMMY_EXPENSES} />
       </main>
     </>
