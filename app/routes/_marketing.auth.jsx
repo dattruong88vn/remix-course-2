dd87484851719aba058077499073b388426be51a
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-05-29 17:13:33 +0700

    77. Visual feedback for  authentication form

--- app/routes/_marketing.auth.jsx
+++ app/routes/_marketing.auth.jsx
@@ -14,3 +14,19 @@ export default AuthPage;
 export function links() {
   return [{ rel: "stylesheet", href: authStyles }];
 }
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get("mode") || "login";
+
+  const formData = await request.formData();
+  const credentials = Object.fromEntries(formData);
+
+  // validate user input
+
+  if (authMode === "login") {
+    // login logic
+  } else {
+    // signup logic
+  }
+}
